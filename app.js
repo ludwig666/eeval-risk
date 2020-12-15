59643e15d35f13fb1c976893876f4ccfcb1b5bd4
ludwig666 <ludwig666@example.com>
2020-12-15 19:45:50 +0900

    add list

--- app.js
+++ app.js
@@ -7,3 +7,32 @@ const HelloVueApp = {
 };
 
 Vue.createApp(HelloVueApp).mount('#app');
+
+const Short = {
+  data() {
+    return {
+      items: [110, 112, 113, 114, 115, 116, 117, 118, 119]
+    }
+  },
+  methods: {
+    shuffle() {
+      this.items = _.shuffle(this.items)
+    }
+  }
+};
+
+Vue.createApp(Short).mount('#short');
+const Long = {
+  data() {
+    return {
+      items: [100, 102, 103, 104, 105, 106, 107, 108, 109]
+    }
+  },
+  methods: {
+    shuffle() {
+      this.items = _.shuffle(this.items)
+    }
+  }
+};
+
+Vue.createApp(Long).mount('#long');
