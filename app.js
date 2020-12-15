55c2532513b733aab191f0a430dd499348ed8a2a
ludwig666 <ludwig666@example.com>
2020-12-15 20:23:17 +0900

    prettier

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-const HelloVueApp = {
+const Order = {
   data() {
     return {
-      message: 'Hello Vue!!'
-    }
-  }
+      price: 104.3,
+    };
+  },
+  methods: {
+    buy() {
+      alert("buy");
+    },
+    sell() {
+      alert("sell");
+    },
+  },
 };
 
-Vue.createApp(HelloVueApp).mount('#app');
+Vue.createApp(Order).mount("#order");
 
 const Short = {
   data() {
     return {
-      items: [110, 112, 113, 114, 115, 116, 117, 118, 119]
-    }
+      items: [110, 112, 113, 114, 115, 116, 117, 118, 119],
+    };
   },
   methods: {
     shuffle() {
-      this.items = _.shuffle(this.items)
-    }
-  }
+      this.items = _.shuffle(this.items);
+    },
+  },
 };
 
-Vue.createApp(Short).mount('#short');
+Vue.createApp(Short).mount("#short");
+
 const Long = {
   data() {
     return {
-      items: [100, 102, 103, 104, 105, 106, 107, 108, 109]
-    }
+      items: [100, 102, 103, 104, 105, 106, 107, 108, 109],
+    };
   },
   methods: {
     shuffle() {
-      this.items = _.shuffle(this.items)
-    }
-  }
+      this.items = _.shuffle(this.items);
+    },
+  },
 };
 
-Vue.createApp(Long).mount('#long');
+Vue.createApp(Long).mount("#long");
