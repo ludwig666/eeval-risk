084ab916c33cdc5afb68ea354d185173a098c169
ludwig666 <ludwig666@example.com>
2020-12-16 22:41:26 +0900

    add fill_arc study

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ class Position {
   }
 }
 
+function radians_per_ratio(ratio) {
+  return 2 * Math.PI * ratio;
+}
+
+function fill_arc(ratio) {
+  var x = 50 + 50 * Math.sin(radians_per_ratio(ratio));
+  var y = 50 - 50 * Math.cos(radians_per_ratio(ratio));
+  var large_arc = ratio > 0.5 ? 1 : 0;
+  return `M 50 50 L 50 0 A 50 50 0 ${large_arc} 1 ${x} ${y} L 50, 50`;
+}
+
 const Order = {
   data() {
     return {
@@ -24,6 +35,32 @@ const Order = {
     reset() {
       this.short = [];
       this.long = [];
+    },
+  },
+  computed: {
+    d: function () {
+      var ratio = this.price / 100.0;
+      if (this.price > 100.0) {
+        ratio = 0.999999;
+      }
+
+      if (this.price < 0) {
+        ratio = 0.0;
+      }
+
+      return fill_arc(ratio);
+    },
+    d2: function () {
+      var ratio = (2 * this.price) / 100.0;
+      if (this.price > 100.0) {
+        ratio = 0.999999;
+      }
+
+      if (this.price < 0) {
+        ratio = 0.0;
+      }
+
+      return fill_arc(ratio);
     },
   },
 };
